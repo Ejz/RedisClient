c73b360ebe70ea624ae3d7c89472b75185594149
Evgeny Cernisev <ejz@ya.ru>
2019-03-17 00:46:21 +0300

    _

--- tests/RedisClientTest.php
+++ tests/RedisClientTest.php
@@ -6,10 +6,50 @@ use PHPUnit\Framework\TestCase;
 class RedisClientTest extends TestCase
 {
     /**
+     *
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->redis = new RedisClient();
+    }
+
+    /**
      * @test
      */
-    public function redis_client_common()
+    public function test_redis_client_ping()
     {
-        $this->assertTrue(true);
+        $this->assertTrue((string) $this->redis->ping() === 'PONG');
+    }
+
+    /**
+     * @test
+     */
+    public function test_redis_client_hgetall()
+    {
+        $this->redis->HMSET('key', 'k1', 'v1', 'k2', 'v2');
+        $this->assertTrue($this->redis->HGETALL('key') === ['k1' => 'v1', 'k2' => 'v2']);
+    }
+
+    /**
+     * @test
+     */
+    public function test_redis_client_eval()
+    {
+        foreach (range(time(), time() + 100) as $i) {
+            $ret = $this->redis->EVAL('return ' . $i, 0);
+            $this->assertTrue($ret === $i);
+        }
+        $ret = $this->redis->EVAL('return {1,2,{3,{"4",5}}}', 0);
+        $this->assertTrue($ret === [1, 2, [3, ['4', 5]]]);
+    }
+
+    /**
+     * @test
+     */
+    public function test_redis_client_exists()
+    {
+        $ret = $this->redis->EXISTS(md5(time()));
+        $this->assertTrue($ret === 0);
     }
 }
