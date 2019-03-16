15352ba9487a07c8034f4392ece4b097d151e16a
Evgeny Cernisev <ejz@ya.ru>
2019-03-17 00:27:07 +0300

    _

---
+++ tests/RedisClientTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Ejz\RedisClient;
+use PHPUnit\Framework\TestCase;
+
+class RedisClientTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function redis_client_common()
+    {
+        $this->assertTrue(true);
+    }
+}
